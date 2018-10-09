00aa89317464ded892c76f022f5aba9223ef81bb
rousor <vw5yymst@gmail.com>
2018-10-09 06:21:09 +0000

    rakuten api

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -14,10 +14,10 @@
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                         <li>
-                            <a href="#">
-                                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-                                アイテムを追加
-                              </a>
+                            <a href="{{ route('items.create') }}">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                            アイテムを追加
+                            </a>
                         </li>
 
                         <li class="dropdown">
